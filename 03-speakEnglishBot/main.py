eb3c75a56001d4f629647adbf80ca9f67d9e0fc7
Anvar Narzullaev <anvarbek@gmail.com>
2021-07-23 12:48:37 +0800

    speakEnglish bot

---
+++ 03-speakEnglishBot/main.py
@@ -0,0 +1,54 @@
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+from oxfordLookup import getDefinitions
+from googletrans import Translator
+translator = Translator()
+
+API_TOKEN = '1636855311:AAHXMC6poH_9JAV6bBPfhL9cN1R9eFhcvUY'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def tarjimon(message: types.Message):
+    print(message)
+    lang = translator.detect(message.text).lang
+    if len(message.text.split()) > 2:
+        dest = 'uz' if lang == 'en' else 'en'
+        await message.reply(translator.translate(message.text, dest).text)
+    else:
+        if lang=='en':
+            word_id = message.text
+        else:
+            word_id = translator.translate(message.text, dest='en').text
+
+        lookup = getDefinitions(word_id)
+        if lookup:
+            await message.reply(f"Word: {word_id} \nDefinitions:\n{lookup['definitions']}")
+            if lookup.get('audio'):
+                await message.reply_voice(lookup['audio'])
+        else:
+            await message.reply("Bunday so'z topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ 03-speakEnglishBot/oxfordLookup.py
@@ -0,0 +1,29 @@
+import requests
+
+app_id = "62b446b5"
+app_key = "c2fe6960887f53b88999a767283d7e34"
+language = "en-gb"
+
+def getDefinitions(word_id):
+    url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+    res = r.json()
+    if 'error' in res.keys():
+        return False
+
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+    definitions = []
+    for sense in senses:
+        definitions.append(f"👉 {sense['definitions'][0]}")
+    output['definitions'] = "\n".join(definitions)
+
+    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
+        output['audio'] = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
+
+    return output
+
+if __name__ == '__main__':
+    from pprint import pprint as print
+    print(getDefinitions('Great Britain'))
+    print(getDefinitions('sdsdsdsds'))
